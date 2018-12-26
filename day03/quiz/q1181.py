33d0c337be62a620afa998632a3ad9b9ad934379
Junwoo Hwang <no.water.in.the.house@gmail.com>
2018-12-26 02:51:46 +0000

    q1181.py

--- day03/quiz/q1181.py
+++ day03/quiz/q1181.py
@@ -1,3 +1,3 @@
 arr = ['but', 'i', 'wont', 'hesitate', 'no', 'more', 'no', 'more', 'it', 'cannot', 'wait', 'im', 'yours']
-l_sorted=sorted(sorted(arr), key=lambda s: len(s))
+l_sorted=sorted(sorted(arr), key=len)
 print(l_sorted)
